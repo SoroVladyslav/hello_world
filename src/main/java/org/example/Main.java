47c6574d842fe651630ecae6b2554708e929bf5e
SoroVladyslav <last.ash2020@gmail.com>
2023-12-03 18:09:16 +0200

    Revert "Still testing"
    This reverts commit bb510b34e775246450fb8a368d389dbeec41bf94.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing....");
-        System.out.println("Still testing...");
+        System.out.println("Testing......");
     }
 }
